70c16dd6a6292ad5076a26500bf35f20a8dccacc
Your Name <youremail@example.com>
2025-03-17 12:17:19 +0530

    batch component created

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  private verifyUrl ='https://localhost:7265/api/User/Verify-student';
+  private studentsUrl = 'https://localhost:7265/api/Student/Get-All-Students';
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
-  constructor() { }
+      // Verify student by ID
+      verifyStudent(studentId: number): Observable<any> {
+        return this.http.post(`${this.verifyUrl}?studentId=${studentId}`,{});
+      }
+  getAllStudents(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.studentsUrl}`);
+  }
 }
