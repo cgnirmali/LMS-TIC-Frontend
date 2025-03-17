71a91cd5f63c091aac09b6f84d3d07362fd73c73
ut01107 <ut01107@UNICOMTIC11>
2025-03-17 10:08:15 +0530

    Implemented a responsive sidebar for Admin, Lecturer, and Student

--- src/app/pages/admin/admin-layout/admin-layout.component.ts
+++ src/app/pages/admin/admin-layout/admin-layout.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [RouterOutlet,CommonModule,ReactiveFormsModule,RouterLink],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
 export class AdminLayoutComponent {
+  isOpen = true;
 
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
+
+  constructor
+  (
+    private router:Router
+  ) {}
+
+
+  logout() {
+    // Clear local storage
+    localStorage.clear();
+
+    // Redirect to the home page
+    this.router.navigate(['/']);
+  }
 }
--- src/app/pages/lecturer/lecturer-layout/lecturer-layout.component.ts
+++ src/app/pages/lecturer/lecturer-layout/lecturer-layout.component.ts
@@ -1,11 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-lecturer-layout',
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './lecturer-layout.component.html',
   styleUrl: './lecturer-layout.component.css'
 })
-export class LecturerLayoutComponent {
+export class LecturerLayoutComponent  {
+  isOpen = true;
+  userName: string = '';
+  userId: string='';
+
+
+
+  constructor(
+    private router:Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.userId = localStorage.getItem('UserId') || ''; // Get the logged-in user's ID from localStorage
+    console.log('userId from localStorage:', this.userId);
+    if (this.userId) {
+      this.getTeacherInfo(this.userId);  // Fetch student data using studentid
+    }
+  }
+
+  getTeacherInfo(teacherid: string) {
+    
+  }
+
+  logout() {
+    // Clear local storage
+    localStorage.clear();
+
+    // Redirect to the home page
+    this.router.navigate(['/']);
+  }
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;  // Toggle sidebar visibility
+  }
+
 
 }
